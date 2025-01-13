f05c99a082629623fd737b7ea06bec2094364bdf
Blobby2191 <87619169+Blobby2191@users.noreply.github.com>
2025-01-12 21:23:58 -0500

    Load form instantly on 'kittenexplosion'.

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function wow() {
                     text = "<h3> Merry Christmas </h3> <p> Christmas Break has just started! There will be no school until the 6th of January.</p> <b> Upcoming tests: Test V, Test VI (Semester Exam) / Special days: Back to School (6 Jan) </b>";
                     break;
                 case "kittenexplosion":
-                    text= "<a href='https://forms.gle/LuVAdvBKgkWUbNau9'> Click here </a>" ;
+                    text= "<iframe height='432' width='288' src='https://forms.gle/LuVAdvBKgkWUbNau9'> </iframe>" ;
                     break;
                 case null:
                     text = "<p> Please enter a code. </p>"
