6853c07674bd57c6dc7620f4288df04576c1f5e1
Blobby2191 <87619169+Blobby2191@users.noreply.github.com>
2025-03-03 10:19:38 -0500

    Update main.js

--- main.js
+++ main.js
@@ -3,19 +3,19 @@ function wow() {
             let x = document.getElementById("code").value;
             switch(x) {
                 case "newsof7s":
-                    text = "<h3> Its March Break Bro </h3>";
-                    break;
+                        text = "<h3> Its March Break Bro </h3>";
+                        break;
                 case "kittenexplosion":
-                    text= "<iframe height='576' width='864' src='https://forms.gle/LuVAdvBKgkWUbNau9'> </iframe>" ;
-                    break;
+                        text= "<iframe height='576' width='864' src='https://forms.gle/LuVAdvBKgkWUbNau9'> </iframe>" ;
+                        break;
                 case "watchmebackflip":
-                        window.location.href = "videohome.html";
+                        text = "<iframe height='576' width='864' src='videohome.html'> </iframe>";
                         break;
                 case "":
-                    text = "<p> Please enter a code. </p>"
-                    break;
+                        text = "<p> Please enter a code. </p>"
+                        break;
                 default:
-                    text = "<p> Your secret code has expired. </p>";
-                    break;
+                        text = "<p> Your secret code has expired. </p>";
+                        break;
 }     document.getElementById("start").innerHTML = text;
 }
